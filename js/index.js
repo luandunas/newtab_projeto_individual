f3d33ccb38de91b43062128d3eac73ad17d70eff
Luan <luandunaas@gmail.com>
2021-10-16 09:58:21 -0300

    Foi ajustado o menu para melhor componentização e boa práticas de responsividade web

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function abrirSideNav() {
+    document.getElementsByClassName("menu")[0].classList.add("openMenu")
+}
+
+function fecharSideNav() {
+    document.getElementsByClassName("menu")[0].classList.remove("openMenu")
+}
